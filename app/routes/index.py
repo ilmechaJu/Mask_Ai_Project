68ed7b428d247c6337f61567cf6b59ef17a3d4da
ChanYoungHan <sanbon0812@naver.com>
2022-11-14 20:27:02 +0900

    add root router

--- app/routes/index.py
+++ app/routes/index.py
@@ -5,16 +5,19 @@ from starlette.responses import Response
 
 router = APIRouter()
 
+
 @router.get("/")
 async def index():
     """
     ELB 상태 체크용 API
     :return:
     """
-    user = Users(status='activate', name="Hellollo")
-    session.add(user)
-    session.commit()
+    # user = Users(status='activate', name="Hellollo")
+    # session.add(user)
+    # session.commit()
 
-    Users().create(session, auto_commit=True, name= "코알라")
+    # Users().create(session, auto_commit=True, name= "코알라")
     current_time = datetime.utcnow()
-    return Response(f"Notification API (UTC: {current_time.strftime('%Y.%m.%d %H:%M:%S')})")
+    return Response(
+        f"Notification API (UTC: {current_time.strftime('%Y.%m.%d %H:%M:%S')})"
+    )
--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from dataclasses import asdict
 from typing import Optional
 
 import uvicorn
-from fastapi import FastAPI, UploadFile, File 
+from fastapi import FastAPI, UploadFile, File
 import shutil
+
 # from app.database.conn import db
 # from app.common.config import conf
-# from app.routes import index, auth
+from app.routes import index
 
 
 def create_app():
-    
+
     app = FastAPI()
     # c = conf()
     # conf_dict = asdict(c)
@@ -22,7 +23,7 @@ def create_app():
     # 미들웨어 정의
 
     # 라우터 정의
-    # app.include_router(index.router)
+    app.include_router(index.router)
     # app.include_router(auth.router, tags=["Authentication"], prefix="/auth")
     return app
 
@@ -32,8 +33,9 @@ app = create_app()
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
 
+
 @app.post("/img")
 async def root(file: UploadFile = File(...)):
-    with open(f'{file.filename}', "wb") as buffer:
+    with open(f"{file.filename}", "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     return {"file_name": file.filename}
