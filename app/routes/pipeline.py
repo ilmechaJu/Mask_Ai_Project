1221ce9c027472b6684774e0f78f84e75b357937
ilmechaJu <pawor006@naver.com>
2022-11-15 18:38:24 +0900

    making image folder path in develop

--- app/routes/pipeline.py
+++ app/routes/pipeline.py
@@ -1,17 +1,22 @@
+import os
 import shutil
 from datetime import datetime
 
-from fastapi import UploadFile, File
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 from starlette.responses import Response
 
 router = APIRouter()
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+IMG_DIR = os.path.join(BASE_DIR, 'images/')
+SERVER_IMG_DIR = os.path.join('http://localhost:8000/','images/')
 
 @router.post("/pipeline/start_detection", tags=["Pipeline"])
 async def start_detection(image: UploadFile = File(...), image_info: str = ""):
     with open(f"{image.filename}", "wb") as buffer:
         shutil.copyfileobj(image.file, buffer)
+    IMG_DIR
+    SERVER_IMG_DIR
     return {"file_name": image.filename}
 
 
